82be443d0d66680a595f52097553193f444c6172
micheaol <micheaol80@gmail.com>
2022-03-24 08:46:58 +0000

    Fix linter

--- app/controllers/api/V1/greetings_controller.rb
+++ app/controllers/api/V1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-        greetings = Regard.find(Regard.pluck(:id).sample)
-        render json: greetings.to_json
-    end
+  def index
+    greetings = Regard.find(Regard.pluck(:id).sample)
+    render json: greetings.to_json
+  end
 end
